55a097414298b423c02f4e6e3b3021a8d1b51818
Nguyen Sy Hien <136237596+NguyenSyHien@users.noreply.github.com>
2025-06-14 23:57:33 +0100

    Simple Ant Logo Based on Reference Image

--- components/OpenAntsLogo.tsx
+++ components/OpenAntsLogo.tsx
@@ -13,23 +13,23 @@ export default function OpenAntsLogo({ size = 32, color = '#8B5CF6' }: OpenAntsL
       <View style={[
         styles.antHead,
         {
-          width: size * 0.35,
-          height: size * 0.35,
+          width: size * 0.4,
+          height: size * 0.4,
           backgroundColor: color,
-          left: size * 0.1,
-          top: size * 0.32,
+          left: size * 0.05,
+          top: size * 0.1,
         }
       ]} />
       
-      {/* Ant thorax - medium oval */}
+      {/* Ant thorax - smaller circle */}
       <View style={[
         styles.antThorax,
         {
-          width: size * 0.28,
-          height: size * 0.4,
+          width: size * 0.25,
+          height: size * 0.25,
           backgroundColor: color,
-          left: size * 0.36,
-          top: size * 0.3,
+          left: size * 0.375,
+          top: size * 0.375,
         }
       ]} />
       
@@ -37,49 +37,100 @@ export default function OpenAntsLogo({ size = 32, color = '#8B5CF6' }: OpenAntsL
       <View style={[
         styles.antAbdomen,
         {
-          width: size * 0.4,
-          height: size * 0.45,
+          width: size * 0.45,
+          height: size * 0.35,
           backgroundColor: color,
-          left: size * 0.55,
-          top: size * 0.275,
+          left: size * 0.5,
+          top: size * 0.55,
         }
       ]} />
       
-      {/* Left antenna - curved line */}
+      {/* Left antenna */}
       <View style={[
         styles.antenna,
         {
-          width: size * 0.2,
-          height: size * 0.04,
+          width: size * 0.15,
+          height: size * 0.03,
           backgroundColor: color,
-          left: size * 0.15,
-          top: size * 0.2,
-          transform: [{ rotate: '-25deg' }],
+          left: size * 0.1,
+          top: size * 0.05,
+          transform: [{ rotate: '-30deg' }],
         }
       ]} />
       
-      {/* Right antenna - curved line */}
+      {/* Right antenna */}
       <View style={[
         styles.antenna,
         {
-          width: size * 0.2,
-          height: size * 0.04,
+          width: size * 0.15,
+          height: size * 0.03,
           backgroundColor: color,
-          left: size * 0.25,
-          top: size * 0.15,
-          transform: [{ rotate: '25deg' }],
+          left: size * 0.3,
+          top: size * 0.05,
+          transform: [{ rotate: '30deg' }],
         }
       ]} />
       
-      {/* Eye - white dot for personality */}
+      {/* Eye - small white circle */}
       <View style={[
         styles.eye,
         {
-          width: size * 0.08,
-          height: size * 0.08,
+          width: size * 0.06,
+          height: size * 0.06,
           backgroundColor: 'white',
-          left: size * 0.22,
-          top: size * 0.42,
+          left: size * 0.18,
+          top: size * 0.22,
+        }
+      ]} />
+      
+      {/* Legs - simple lines */}
+      {/* Front legs */}
+      <View style={[
+        styles.leg,
+        {
+          width: size * 0.12,
+          height: size * 0.02,
+          backgroundColor: color,
+          left: size * 0.35,
+          top: size * 0.45,
+          transform: [{ rotate: '-45deg' }],
+        }
+      ]} />
+      
+      <View style={[
+        styles.leg,
+        {
+          width: size * 0.12,
+          height: size * 0.02,
+          backgroundColor: color,
+          left: size * 0.45,
+          top: size * 0.45,
+          transform: [{ rotate: '45deg' }],
+        }
+      ]} />
+      
+      {/* Back legs */}
+      <View style={[
+        styles.leg,
+        {
+          width: size * 0.12,
+          height: size * 0.02,
+          backgroundColor: color,
+          left: size * 0.55,
+          top: size * 0.75,
+          transform: [{ rotate: '-45deg' }],
+        }
+      ]} />
+      
+      <View style={[
+        styles.leg,
+        {
+          width: size * 0.12,
+          height: size * 0.02,
+          backgroundColor: color,
+          left: size * 0.65,
+          top: size * 0.75,
+          transform: [{ rotate: '45deg' }],
         }
       ]} />
     </View>
@@ -107,6 +158,10 @@ const styles = StyleSheet.create({
     borderRadius: 1000,
   },
   eye: {
+    position: 'absolute',
+    borderRadius: 1000,
+  },
+  leg: {
     position: 'absolute',
     borderRadius: 1000,
   },
